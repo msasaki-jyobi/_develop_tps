457e31c5a8e1f0251396c98760a75ac6c3037218
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2024-10-24 14:43:39 +0900

    Delete namespace RPG, アタッチ Unitparameter(コメント化)

--- Scripts/Unit/TPSUnitController.cs
+++ Scripts/Unit/TPSUnitController.cs
@@ -1,8 +1,6 @@
 ﻿using develop_common;
 using DG.Tweening;
 using JetBrains.Annotations;
-using RPGCharacterAnims.Actions;
-using RPGCharacterAnims.Lookups;
 using System;
 using System.Collections;
 using System.Collections.Generic;
